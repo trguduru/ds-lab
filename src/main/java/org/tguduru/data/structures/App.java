6898308f593f350861264d39e405dd08fe1e7914
tguduru <guduru.thirupathireddy@gmail.com>
2016-05-30 22:23:39 -0500

    added project

---
+++ src/main/java/org/tguduru/data/structures/App.java
@@ -0,0 +1,13 @@
+package org.tguduru.data.structures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
