62bf0db267e9b87f4d26370343a578afbb8deb9d
tguduru <guduru.thirupathireddy@gmail.com>
2016-06-07 22:32:48 -0500

    added simple arrays usage

---
+++ src/main/java/org/tguduru/data/structures/arrays/GameEntry.java
@@ -0,0 +1,34 @@
+package org.tguduru.data.structures.arrays;
+
+/**
+ * Provides details about a person with name and his score in a game.
+ *
+ * @author Guduru, Thirupathi Reddy
+ * @modified 5/30/16.
+ */
+public class GameEntry {
+    private String name;
+    private int score;
+
+    public GameEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GameEntry{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", score=").append(score);
+        sb.append('}');
+        return sb.toString();
+    }
+}
