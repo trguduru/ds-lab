5e78ade734c0820c3e5710b134493cdde652e7e3
tguduru <guduru.thirupathireddy@gmail.com>
2016-06-19 19:34:20 -0500

    strings examples

---
+++ src/main/java/org/tguduru/data/structures/strings/AnagramsPair.java
@@ -0,0 +1,19 @@
+package org.tguduru.data.structures.strings;
+
+/**
+ * Find out the pair of words if they follow anagram rule, example {car,ars},{star,rats}.
+ * Given:
+ *  list of words
+ * Result:
+ *  Set of anagram pairs.
+ *
+ * Algorithm:
+ *
+ * @author Guduru, Thirupathi Reddy
+ * @modified 6/15/16.
+ */
+public class AnagramsPair {
+    public static void main(String[] args) {
+        System.out.println("car".getBytes() + " , " + "arc".getBytes());
+    }
+}
